d9b1a0282e5b2190cb7cebc2b64bb40bb36e32b4
N153211 <ponmaran@gmail.com>
2012-10-11 17:16:08 -0700

    User input successfully captured.

--- MyFirstApp/gen/com/example/myfirstapp/R.java
+++ MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -28,6 +28,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int bridgeNum=0x7f040007;
         public static final int button_send=0x7f040002;
         public static final int edit_message=0x7f040001;
         public static final int hello_world=0x7f040005;
--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfirstapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,11 +12,27 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Get shared preferences
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), "");
+        String message = new String();
+        if (bridgeNum.equals(""))
+        	{
+	            setContentView(R.layout.activity_main);
+//	            message = "Your input \"" + message + "\" is saved";
+        	}
+        else
+        	{
+            	message = bridgeNum;
+                Intent intent = new Intent(this, DisplayMessageActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+        	};
     }
 
     @Override
@@ -25,10 +43,23 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+/*        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+*/
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.bridgeNum), message);
+        editor.commit();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, "Your message \"" + message + "\" is saved");
+        startActivity(intent);
+
+	}
 }
