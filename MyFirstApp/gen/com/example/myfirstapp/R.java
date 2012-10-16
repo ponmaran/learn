fe0f7802f1de9e9f279c8674ee1a1a1bdfa8b61e
N153211 <ponmaran@gmail.com>
2012-10-15 17:49:06 -0700

    Layout completed. Issue with saving multiple preferences. Last pref in the code applies to both pref.

--- MyFirstApp/gen/com/example/myfirstapp/R.java
+++ MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -15,8 +15,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int delay_time=0x7f070001;
-        public static final int edit_message=0x7f070000;
+        public static final int edit_bridge_num=0x7f070000;
+        public static final int edit_delay=0x7f070001;
         public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
@@ -31,9 +31,13 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int bridgeNum=0x7f040007;
         public static final int button_done=0x7f040002;
-        public static final int edit_message=0x7f040001;
+        public static final int config_page_message=0x7f04000a;
+        public static final int delayTime=0x7f040009;
+        public static final int edit_bridge_num_hint=0x7f040001;
+        public static final int edit_delay_hint=0x7f040008;
         public static final int hello_world=0x7f040005;
         public static final int menu_settings=0x7f040003;
+        public static final int shared_prefs_file=0x7f04000b;
         public static final int title_activity_display_message=0x7f040006;
         public static final int title_activity_main=0x7f040004;
     }
--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -13,33 +13,33 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Get shared preferences
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
         String bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), "");
-//        String message = new String();
+        String delayTime = sharedPref.getString(getString(R.string.delayTime), "0");
+
         if (bridgeNum.equals(""))
         	{
-//	            message = "Your input \"" + message + "\" is saved";
             Intent intent = new Intent(this, DisplayMessageActivity.class);
             intent.putExtra(EXTRA_MESSAGE, "No Data");
             startActivity(intent);
         	}
         else
         	{
-//			message = bridgeNum;
 //          TextView textView = new TextView(this);
-            EditText editText = (EditText) findViewById(R.id.edit_message);
+            EditText editText1 = (EditText) findViewById(R.id.edit_bridge_num);
 //          editText.setTextSize(15);
-            editText.setText(bridgeNum, TextView.BufferType.NORMAL);
-//            setContentView(R.layout.activity_main);
+            editText1.setText(bridgeNum, TextView.BufferType.NORMAL);
+            EditText editText2 = (EditText) findViewById(R.id.edit_delay);
+            editText2.setText(delayTime, TextView.BufferType.NORMAL);
+
         	}; 
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
     }
 
     @Override
@@ -48,24 +48,37 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private void savePrefs(String key, String value){
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, value);
+        editor.commit();
+       }
+
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
+    public void editDone(View view) {
 /*        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 */
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(getString(R.string.bridgeNum), message);
-        editor.commit();
+        EditText editText1 = (EditText) findViewById(R.id.edit_bridge_num);
+        String bridgeNum = editText1.getText().toString();
+        savePrefs(getString(R.string.bridgeNum), bridgeNum);
+        
+        EditText editText2 = (EditText) findViewById(R.id.edit_delay);
+        String delayTime = editText2.getText().toString();        
+        savePrefs(getString(R.string.delayTime), delayTime);
+
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
+        bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), "");
+        delayTime = sharedPref.getString(getString(R.string.delayTime), "0");
+        
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
 //        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, "Your message \"" + message + "\" is saved");
+        intent.putExtra(EXTRA_MESSAGE, "Bridge Number \"" + bridgeNum + "\" and " + "Dealy \"" + delayTime + "\" are saved.");
         startActivity(intent);
 
 	}
