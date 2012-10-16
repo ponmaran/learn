ee5015606ed7c64e2d0e8b1b7c8c8c4dcdde6128
Ponmaran <Ponmaran@gmail.com>
2012-10-16 00:02:16 -0700

    1. User input from 2 fields captured and retrieved successfully. 2. Reset button added. 3. Calls to DisplayMessageActivity are commented out in the MainActivity.

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.myfirstapp;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,26 +19,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         // Get shared preferences
         SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
-        String bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), "");
-        String delayTime = sharedPref.getString(getString(R.string.delayTime), "0");
+        String bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), getString(R.string.bridge_default_value));
+        String delayTime = sharedPref.getString(getString(R.string.delayTime), getString(R.string.delay_default_value));
 
         if (bridgeNum.equals(""))
         	{
-            Intent intent = new Intent(this, DisplayMessageActivity.class);
+/*            Intent intent = new Intent(this, DisplayMessageActivity.class);
             intent.putExtra(EXTRA_MESSAGE, "No Data");
             startActivity(intent);
-        	}
+*/        	}
         else
         	{
-//          TextView textView = new TextView(this);
             EditText editText1 = (EditText) findViewById(R.id.edit_bridge_num);
-//          editText.setTextSize(15);
             editText1.setText(bridgeNum, TextView.BufferType.NORMAL);
             EditText editText2 = (EditText) findViewById(R.id.edit_delay);
             editText2.setText(delayTime, TextView.BufferType.NORMAL);
 
         	}; 
-//        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
+
     }
 
     @Override
@@ -48,38 +45,41 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    private void savePrefs(String key, String value){
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(key, value);
-        editor.commit();
-       }
-
     /** Called when the user clicks the Send button */
     public void editDone(View view) {
-/*        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-*/
+
         EditText editText1 = (EditText) findViewById(R.id.edit_bridge_num);
         String bridgeNum = editText1.getText().toString();
-        savePrefs(getString(R.string.bridgeNum), bridgeNum);
-        
+
         EditText editText2 = (EditText) findViewById(R.id.edit_delay);
         String delayTime = editText2.getText().toString();        
-        savePrefs(getString(R.string.delayTime), delayTime);
 
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
-        bridgeNum = sharedPref.getString(getString(R.string.bridgeNum), "");
-        delayTime = sharedPref.getString(getString(R.string.delayTime), "0");
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.bridgeNum), bridgeNum);
+        editor.putString(getString(R.string.delayTime), delayTime);
+        editor.commit();
         
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_bridge_num);
-//        String message = editText.getText().toString();
+/*        Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, "Bridge Number \"" + bridgeNum + "\" and " + "Dealy \"" + delayTime + "\" are saved.");
         startActivity(intent);
+*/
+	}
+    
+    /** Called when the user clicks the Reset button */
+    public void pressReset(View view) {
 
+        EditText editText1 = (EditText) findViewById(R.id.edit_bridge_num);
+        editText1.setText(R.string.bridge_default_value, TextView.BufferType.NORMAL);
+
+        EditText editText2 = (EditText) findViewById(R.id.edit_delay);
+        editText2.setText(R.string.delay_default_value, TextView.BufferType.NORMAL);
+
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.shared_prefs_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.bridgeNum), getString(R.string.bridge_default_value));
+        editor.putString(getString(R.string.delayTime), getString(R.string.delay_default_value));
+        editor.commit();
+        
 	}
 }
